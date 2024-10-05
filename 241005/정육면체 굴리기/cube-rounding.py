927660757acb0ced7bc71fe77cb28bad5fbd7fe0
gravity0717 <1357vvv@naver.com>
2024-10-05 23:50:45 +0900

    Title: 정육면체 굴리기, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241005/정육면체 굴리기/cube-rounding.py
+++ 241005/정육면체 굴리기/cube-rounding.py
@@ -4,58 +4,49 @@ n, m, x, y, k = map(int, sys.stdin.readline().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 directions =  list(map(int, input().split()))
 dice = [0] * 6
+dx , dy = [0, 0, 0, 1, -1], [0, 1, -1, 0, 0] # arr[y][x] 이렇게 하는 대신 변위를 뒤집자 
 
-def move_east(dice, x, y):
-    if 0 <= x+1 < m:
-    #   0    1    2  3  4   5
-        bot, top, west, east, south, north = dice 
-        # roll to east 
-        dice[0], dice[1], dice[2], dice[3] = east, west, bot, top 
-        x += 1 
-        return dice, x, y, True
-    return dice, x, y, False
+def is_in_range(x,y):
+    return 0<=x and x < n and 0<= y and y<m
 
-def move_west(dice, x, y):
-    if 0 <= x - 1 < m: 
+def roll_east(dice):
     #   0    1    2  3  4   5
-        bot, top, west, east, south, north = dice 
-        # roll to west 
-        dice[0], dice[1], dice[2], dice[3] = west, east, top, bot
-        x -= 1 
-        return dice, x, y, True
-    return dice, x, y, False
+    bot, top, west, east, south, north = dice 
+    # roll to east 
+    dice[0], dice[1], dice[2], dice[3] = east, west, bot, top 
+    return dice
+
+def roll_west(dice):
+    #   0    1    2  3  4   5
+    bot, top, west, east, south, north = dice 
+    # roll to west 
+    dice[0], dice[1], dice[2], dice[3] = west, east, top, bot
+    return dice
     
-def move_north(dice, x, y):
-    if 0 <= y - 1 < n:
-        #   0    1    2  3  4   5
-        bot, top, west, east, south, north = dice 
-        # roll to east 
-        dice[0], dice[1], dice[4], dice[5] = north, south, bot, top
-        y -= 1 
-        return dice, x, y, True
-    return dice, x, y, False
+def roll_north(dice):
+    #   0    1    2  3  4   5
+    bot, top, west, east, south, north = dice 
+    # roll to east 
+    dice[0], dice[1], dice[4], dice[5] = north, south, bot, top
+    return dice
 
-def move_south(dice, x, y):
-    if 0 <= y + 1 < n:
-        #   0    1    2  3  4   5
-        bot, top, west, east, south, north = dice 
-        # roll to east 
-        dice[0], dice[1], dice[4], dice[5] = south, north, top, bot
-        y += 1 
-        return dice, x, y, True
-    return dice, x, y, False
+def roll_south(dice):
+    #   0    1    2  3  4   5
+    bot, top, west, east, south, north = dice 
+    # roll to east 
+    dice[0], dice[1], dice[4], dice[5] = south, north, top, bot
+    return dice
 
-move_funcs = [None, move_east, move_west, move_north, move_south]
+roll_funcs = [None, roll_east, roll_west, roll_north, roll_south]
 for d in directions:
-    dice, x, y, flag = move_funcs[d](dice, x, y)
+    nx, ny = x + dx[d], y + dy[d]
+    if is_in_range(nx,ny):
+        x, y = nx, ny 
+        dice = roll_funcs[d](dice)
+        if board[x][y] == 0:
+            board[x][y] = dice[0]
+        else:
+            dice[0] = board[x][y]
+            board[x][y] = 0 
     
-    if flag == False:
-        continue
-
-    if board[y][x] == 0:
-        board[y][x] = dice[0]
-    else:
-        dice[0] = board[y][x]
-        board[y][x] = 0 
-
-    print(dice[1])
+        print(dice[1])
